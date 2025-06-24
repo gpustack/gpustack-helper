f180da2a5440dc6790af7168f5708a1c83d5af0a
Yuxing Deng <jxfa0043379@hotmail.com>
2025-06-25 02:14:12 +0800

    fix: cacert is not set in GH builds

--- gpustack_helper/binary_entrypoint.py
+++ gpustack_helper/binary_entrypoint.py
@@ -4,6 +4,7 @@ import multiprocessing
 import re
 import sys
 import os
+import certifi
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
@@ -11,6 +12,8 @@ if __name__ == "__main__":
     binary_name = os.path.basename(
         sys.argv[0]
     )  # Ensure the script name is set correctly
+    if os.getenv["SSL_CERT_FILE"] is None:
+        os.environ["SSL_CERT_FILE"] = certifi.where()
     if binary_name == "vox-box":
         sys.exit(vox_box())
     else:
