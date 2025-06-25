bae440acc586fc4d6cd47e33684eafc74a27545b
Yuxing Deng <jxfa0043379@hotmail.com>
2025-06-25 17:42:40 +0800

    feat: add version copy button

--- gpustack_helper/about.py
+++ gpustack_helper/about.py
@@ -1,5 +1,6 @@
 import logging
-from PySide6.QtWidgets import QMessageBox, QWidget
+from PySide6.QtWidgets import QMessageBox, QWidget, QApplication
+from PySide6.QtCore import Qt
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +11,8 @@ class About:
     gpustack_commit: str
     helper_version: str
     helper_commit: str
+    prefix: str = "GPUStack\n"
+    text: str
 
     def __init__(self, parent: QWidget):
         self.parent = parent
@@ -35,10 +38,19 @@ class About:
 
         except Exception as e:
             logger.error("Failed to get GPUStack version or commit", exc_info=e)
+        gpustack_version = f"版本: {self.gpustack_version}({self.gpustack_commit})\n"
+        helper_version = f"Helper: {self.helper_version}({self.helper_commit})"
+        self.text = f"{gpustack_version}{helper_version}"
 
     def exec(self):
-        return QMessageBox.information(
-            self.parent,
-            "关于",
-            f"GPUStack\n版本: {self.gpustack_version}({self.gpustack_commit})\nHelper: {self.helper_version}({self.helper_commit})",
-        )
+        msg = QMessageBox(self.parent)
+        msg.setWindowTitle("关于")
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.setText(f"{self.prefix}{self.text}")
+        copy_button = msg.addButton("复制", QMessageBox.ButtonRole.AcceptRole)
+        msg.addButton("确认", QMessageBox.ButtonRole.RejectRole)
+        msg.setDefaultButton(copy_button)
+        msg.setWindowFlags(msg.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+        msg.exec()
+        if msg.clickedButton() == copy_button:
+            QApplication.clipboard().setText(self.text)
