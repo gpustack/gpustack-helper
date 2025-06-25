eb7b3ca5b0d00de44831b5fd8592b69dfa831ae9
Yuxing Deng <jxfa0043379@hotmail.com>
2025-06-25 11:38:32 +0800

    fix: put ssl cert file into the top of everything

--- gpustack_helper/binary_entrypoint.py
+++ gpustack_helper/binary_entrypoint.py
@@ -1,10 +1,18 @@
+import os
+import certifi
+
+if __name__ == "__main__":
+    # Ensure the SSL_CERT_FILE environment variable is set to the path provided by certifi.
+    # This guarantees that secure connections use the correct certificate file.
+    # It is initialized early to ensure other modules use the correct configuration.
+    if os.getenv("SSL_CERT_FILE", None) is None:
+        os.environ["SSL_CERT_FILE"] = certifi.where()
+
 from gpustack.main import main as gpustack
 from vox_box.main import main as vox_box
 import multiprocessing
 import re
 import sys
-import os
-import certifi
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
@@ -12,8 +20,6 @@ if __name__ == "__main__":
     binary_name = os.path.basename(
         sys.argv[0]
     )  # Ensure the script name is set correctly
-    if os.getenv("SSL_CERT_FILE", None) is None:
-        os.environ["SSL_CERT_FILE"] = certifi.where()
     if binary_name == "vox-box":
         sys.exit(vox_box())
     else:
