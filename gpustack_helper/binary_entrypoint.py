443edf1063902db55c3b9004af753bd0f91104bd
Yuxing Deng <jxfa0043379@hotmail.com>
2025-06-25 09:45:28 +0800

    fix: call getenv with wrong format

--- gpustack_helper/binary_entrypoint.py
+++ gpustack_helper/binary_entrypoint.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     binary_name = os.path.basename(
         sys.argv[0]
     )  # Ensure the script name is set correctly
-    if os.getenv["SSL_CERT_FILE"] is None:
+    if os.getenv("SSL_CERT_FILE", None) is None:
         os.environ["SSL_CERT_FILE"] = certifi.where()
     if binary_name == "vox-box":
         sys.exit(vox_box())
